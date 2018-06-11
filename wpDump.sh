3e9319e450968472203571f9b1a58e645aa0332a
Byidi <contact@byidi.fr>
2018-06-11 09:05:46 +0200

    Move savePath folder to tmp before archiving

--- wpDump.sh
+++ wpDump.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 #
 
+tmpSavePath="/tmp/wpt/wpDump/"
 clear
 
 if [[ -z "$2" ]]; then
@@ -14,6 +15,8 @@ name=${path##*/}
 bddName=${name/ /_}
 saveDate=$(date +%Y%m%d_%H%M%S)
 
+rm -rf "$tmpSavePath"
+mkdir -p "$tmpSavePath"
 
 echo -n "Récupération des informations : "
 bddName=$(cat "$path"/wp-config.php | grep "DB_NAME" | cut -d"'" -f 4)
@@ -27,24 +30,28 @@ echo "- Pass : ************"
 echo "- Adress : "$bddAddress
 
 echo -n "Création des répertoires : "
-mkdir -p "$savePath"/"$name"/"$name"_"$saveDate"/sql
+mkdir -p "$tmpSavePath"/"$name"/"$name"_"$saveDate"/sql
 echo "Done"
 
 echo -n "Exportation des tables : "
 tables=$(MYSQL_PWD="$bddPass" mysql -u "$bddUser" -h "$bddAddress" -e "use "$bddName";show tables;" | tr -d "| " | grep -v "Tables_in_")
 
 for table in $tables; do
-	MYSQL_PWD="$bddPass" mysqldump -u "$bddUser"  "$bddName" --single-transaction --skip-lock-tables "$table" > "$savePath"/"$name"/"$name"_"$saveDate"/sql/"$table".sql
+	MYSQL_PWD="$bddPass" mysqldump -u "$bddUser"  "$bddName" --single-transaction --skip-lock-tables "$table" > "$tmpSavePath"/"$name"/"$name"_"$saveDate"/sql/"$table".sql
 done
 echo "Done"
 
 echo -n "Copie des fichiers : "
-cp -R $path "$savePath"/"$name"/"$name"_"$saveDate"/
+cp -R $path "$tmpSavePath"/"$name"/"$name"_"$saveDate"/
 echo "Done"
 
 echo -n "Compression de la sauvegarde : "
-cd "$savePath"/"$name"/
+cd "$tmpSavePath"/"$name"/
 tar -czf "$name"_"$saveDate".tar.gz "$name"_"$saveDate"/
+if [[ ! -d $savePath ]]; then
+	mkdir -p "$savePath"
+fi
+cp "$name"_"$saveDate".tar.gz "$savePath"
 echo "Done"
 
 echo -n "Nettoyage : "
